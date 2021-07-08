88b8c87095d4ffd27a8bcfcb9146ee10a980ea85
lan <44520215@qq.com>
2021-07-08 01:34:14 +0000

    update docker/crontab_list.sh.燃动夏季

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -138,4 +138,4 @@
 */5 * * * * node /scripts/jd_jddj_fruit_collectWater.js >> /scripts/logs/jd_jddj_fruit_collectWater.log 2>&1
 
 #燃动夏季
-12 9,11,13,15,17 * * * /scripts/jd_summer_movement.js >> /scripts/logs/jd_summer_movement.log 2>&1
+12 9,11,13,15,17 * * * node /scripts/jd_summer_movement.js >> /scripts/logs/jd_summer_movement.log 2>&1
