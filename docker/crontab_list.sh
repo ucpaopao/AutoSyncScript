80479f5666894fb2f179e7a5920f9d1f988a0310
lan <44520215@qq.com>
2021-07-09 07:38:34 +0000

    update docker/crontab_list.sh.

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -119,7 +119,7 @@
 #京喜牧场
 15 0,12,22 * * * node /scripts/jd_jxmc.js >> /scripts/logs/jd_jxmc.log 2>&1
 #天天加速
-8 0-23/3 * * * node /scripts/jd_speed.js >> /scripts/logs/jd_speed.log 2>&1
+0 0-23/3 * * * node /scripts/jd_speed.js >> /scripts/logs/jd_speed.log 2>&1
 #京东健康兑换商品
 0 0 * * * node /scripts/jd_star_health_change.js >> /scripts/logs/jd_star_health_change.log 2>&1
 #京喜工厂开团
