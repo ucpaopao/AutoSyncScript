cdcc67a0a380780d646e7e9a0f85ce0b09f1b405
iroyway <iroyway@gmail.com>
2021-08-11 01:20:23 +0800

    删除京东到家

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -130,12 +130,7 @@
 
 #点点卷
 36 0,20 * * * node /scripts/jd_necklace.js >> /scripts/logs/jd_necklace.log 2>&1
-#新增京东到家
-10 0,3,8,11,17 * * * node /scripts/jd_jddj_fruit.js >> /scripts/logs/jd_jddj_fruit.log 2>&1
-10 0 * * * node /scripts/jd_jddj_bean.js >> /scripts/logs/jd_jddj_bean.log 2>&1
-10 8 * * * node /scripts/jd_jddj_plantBeans.js >> /scripts/logs/jd_jddj_plantBeans.log 2>&1
-*/5 * * * * node /scripts/jd_jddj_getPoints.js >> /scripts/logs/jd_jddj_getPoints.log 2>&1
-*/5 * * * * node /scripts/jd_jddj_fruit_collectWater.js >> /scripts/logs/jd_jddj_fruit_collectWater.log 2>&1
+
 
 
 #validate.txt 生成
